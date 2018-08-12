e36e265cb4541d9e0d24e5c3727c4654c7426bf5
Mike Joyce <contact@mikejoyce.me>
2018-08-13 02:22:18 +0700

    Upgrade Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,86 @@
 /**
  * @file Gulpfile
- * @file overview Gulp build configuration
  * @author Mike Joyce [hello@mikejoyce.io]
  */
 
 /**
  * Load Gulp
- * @type {object}
- * @external 'require('gulp')'
+ * @type {Object}
+ * @external 'gulp'
  * @see {@link http://gulpjs.com/}
  */
-var gulp = require('gulp');
+const gulp = require('gulp');
 
 /**
- * Load Gulp Path
- * @type {object}
- */
-var path = require('path');
-
-/**
- * Load Node Del
- * @type {object}
- * @external 'require('del')'
+ * Load del
+ * @type {Object}
+ * @external 'del'
  * @see {@link https://www.npmjs.com/package/del}
  */
-var del = require('del');
+const del = require('del');
+
+/**
+ * Load fs
+ * @type {Object}
+ * @external 'fs'
+ * @see {@link https://nodejs.org/api/fs.html#fs_file_system}
+ */
+const fs = require('fs');
+
+/**
+ * Parse command line strings, allows the --production switch
+ * @type {Object}
+ * @external 'yargs'
+ * @see {@link https://www.npmjs.com/package/yargs}
+ */
+const argv = require('yargs').argv;
+
+/**
+ * Load jpeg-recompress imagemin plugin
+ * @type {Object}
+ * @external 'imagemin-jpeg-recompresss'
+ * @see {@link https://www.npmjs.com/package/imagemin-jpeg-recompress}
+ */
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 
 /**
  * Load Gulp Plugins
- * @type {object}
+ * @type {Object}
  * @external 'gulpLoadPlugins'
  * @see {@link https://www.npmjs.com/package/gulp-load-plugins}
  */
-var gulpLoadPlugins = require('gulp-load-plugins');
-var plugins = gulpLoadPlugins({
-	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
-	replaceString: /\bgulp[\-.]/
+const gulpLoadPlugins = require('gulp-load-plugins');
+const $ = gulpLoadPlugins({
+  pattern: [
+    'gulp-*',
+    'gulp.*'
+  ],
+  replaceString: /\bgulp[\-.]/
 });
 
 /**
- * Clean Task.
- * Cleans the build directories
+ * Configuration
+ * @type {Object}
+ */
+const config = {
+  dist: 'dist',
+  packages: 'node_modules',
+  src: 'src'
+};
+
+/**
+ * Clean Task. Cleans the build directories.
  * @external 'del()'
  * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/API.md#gulptaskname--deps-fn}
  */
-gulp.task('clean', function(c) {
-	del(['dist/js/*', 'dist/css/*']);
+gulp.task('clean', function(done) {
+  del([
+    `${config.dist}/css/*`,
+    `${config.dist}/favicons/*`,
+    `${config.dist}/fonts/*`,
+    `${config.dist}/images/*`,
+    `${config.dist}/js/*`
+  ], done());
 });
 
 /**
@@ -53,32 +88,56 @@ gulp.task('clean', function(c) {
  * @external 'gulp.task'
  * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
  */
-gulp.task('scripts', function() {
-	/**
-	 * Gulp Main Bower Files
-	 * @external '.mainBowerFiles'
-	 * @see {@link https://www.npmjs.com/package/gulp-main-bower-files}
-	 */
-	gulp.src(plugins.mainBowerFiles().concat(['src/js/app.js']))
-			/**
-			 * Gulp Filter
-			 * @external '.filter'
-			 * @see {@link https://www.npmjs.com/package/gulp-filter}
-			 */
-			.pipe(plugins.filter('*.js'))
-			/**
-			 * Gulp Concat
-			 * @external '.concat'
-			 * @see {@link https://www.npmjs.com/package/gulp-concat}
-			 */
-			.pipe(plugins.concat('app.js'))
-			/**
-			 * Gulp Uglify
-			 * @external '.uglify'
-			 * @see {@link https://www.npmjs.com/package/gulp-uglify}
-			 */
-			.pipe(plugins.uglify())
-			.pipe(gulp.dest('./dist/js'))
+gulp.task('scripts', function(done) {
+
+  gulp.src([
+    `${config.packages}/jquery/dist/jquery.js`,
+    `${config.packages}/what-input/dist/what-input.js`,
+    `${config.packages}/foundation-sites/dist/js/foundation.js`,
+    `${config.src}/js/**/*.js`
+  ])
+
+  /**
+   * Gulp Concat
+   * @external '.concat'
+   * @see {@link https://www.npmjs.com/package/gulp-concat}
+   */
+  .pipe($.concat('app.js'))
+
+  /**
+   * Gulp Plumber
+   * @external '.plumber'
+   * @see {@link https://www.npmjs.com/package/gulp-plumber
+   */
+  .pipe($.plumber({
+    handleError: function(err) {
+      console.log(err);
+      this.emit('end');
+    }
+  }))
+
+  /**
+   * Gulp If
+   * @external '.if'
+   * @see {@link https://www.npmjs.com/package/gulp-if}
+   */
+  .pipe($.if(argv.prod,
+
+   /**
+    * Gulp Uglify
+    * @external '.uglify'
+    * @see {@link https://www.npmjs.com/package/gulp-uglify}
+    */
+    $.uglify({
+      mangle: true
+    })
+
+  ))
+
+  .pipe(gulp.dest(`${config.dist}/js`))
+
+  done();
+
 });
 
 /**
@@ -86,47 +145,203 @@ gulp.task('scripts', function() {
  * @external 'gulp.task'
  * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
  */
-gulp.task('styles', function() {
-  gulp.src('./src/sass/*.scss')
-	  	/**
-	  	 * Gulp Compass
-	  	 * @external '.compass'
-	  	 * @see {@link https://www.npmjs.com/package/gulp-compass}
-	  	 */
-	    .pipe(plugins.compass({
-	      project: path.join(__dirname, './src'),
-	      css: '../dist/css',
-	      sass: 'sass',
-	      style: 'compressed',
-	      import_path: 'vendor'
-	    }))
-	    .pipe(gulp.dest('../dist/css'))
+gulp.task('styles', function(done) {
+
+  gulp.src(`${config.src}/scss/*.scss`)
+
+  /**
+   * Gulp Sass
+   * @external '.sass'
+   * @see {@link https://www.npmjs.com/package/gulp-sass}
+   */
+  .pipe($.sass({
+    includePaths: [
+      'node_modules/foundation-sites/scss',
+      'node_modules/motion-ui/src',
+      'node_modules/motion-ui/'
+    ],
+    css: './',
+    style: 'expanded',
+    sourceMap: false
+  }).on('error', $.sass.logError))
+
+  /**
+   * Gulp Autoprefixer
+   * @external '.autoprefixer'
+   * @see {@link https://www.npmjs.com/package/gulp-autoprefixer}
+   */
+  .pipe($.autoprefixer({
+    browsers: [
+      'last 2 versions',
+      'ie >= 9',
+      'and_chr >= 2.3'
+    ]
+  }))
+
+  /**
+   * Gulp If
+   * @external '.if'
+   * @see {@link https://www.npmjs.com/package/gulp-if}
+   */
+  .pipe($.if(argv.prod,
+
+    /**
+     * Gulp Clean CSS
+     * @external '.cleanCss'
+     * @see {@link https://www.npmjs.com/package/gulp-clean-css}
+     */
+    $.cleanCss()
+
+  ))
+
+  .pipe(gulp.dest(`${config.dist}/css`))
+
+  done();
+
 });
 
 /**
- * Image Task. Minify images.
+ * Images Task. Minify images.
  * @external 'gulp.task'
  * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
  */
-gulp.task('images', function() {
-	gulp.src('./src/images/*')
-			/**
-			 * Gulp Imagemin
-			 * @external '.imagemin()'
-			 * @see {@link https://www.npmjs.com/package/gulp-imagemin}
-			 */
-			.pipe(plugins.imagemin())
-			.pipe(gulp.dest('./dist/images'))
+gulp.task('images', function(done) {
+
+  gulp.src(`${config.src}/images/*`)
+
+  /**
+   * Gulp Imagemin
+   * @external '.imagemin()'
+   * @see {@link https://www.npmjs.com/package/gulp-imagemin}
+   */
+  .pipe($.imagemin([
+    imageminJpegRecompress({
+      loops: 4,
+      min: 50,
+      max: 95,
+      quality: 'high'
+    })
+  ]))
+
+  .pipe(gulp.dest(`${config.dist}/images`))
+
+  done();
+
 });
 
 /**
- * Fonts Task. Move Font Awesome font files to dist.
+ * Fonts Task. Move font files to dist.
  * @external 'gulp.task'
  * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
  */
-gulp.task('fonts', function() {
-	gulp.src('./src/vendor/font-awesome/fonts/fontawesome-webfont.*')
-			.pipe(gulp.dest('./dist/fonts'))
+gulp.task('fonts', function(done) {
+
+  gulp.src(`${config.src}/fonts/*`)
+
+  .pipe(gulp.dest(`${config.dist}/fonts`))
+
+  done();
+
+});
+
+/**
+ * Favicon Task. Generates a favicon for all major platforms.
+ * @external '.realFavicon'
+ * @see {@link https://www.npmjs.com/package/gulp-real-favicon}
+ */
+gulp.task('favicon', function(done) {
+
+  $.realFavicon.generateFavicon({
+      masterPicture: `${config.src}/favicon/favicon.png`,
+      dest: config.dist + '/favicons',
+      iconsPath: './',
+      design: {
+        ios: {
+          pictureAspect: 'noChange',
+          assets: {
+            ios6AndPriorIcons: false,
+            ios7AndLaterIcons: false,
+            precomposedIcons: false,
+            declareOnlyDefaultIcon: true
+          }
+        },
+        desktopBrowser: {},
+        windows: {
+          pictureAspect: 'noChange',
+          backgroundColor: '#ffffff',
+          onConflict: 'override',
+          assets: {
+            windows80Ie10Tile: false,
+            windows10Ie11EdgeTiles: {
+              small: false,
+              medium: true,
+              big: false,
+              rectangle: false
+            }
+          }
+        },
+        androidChrome: {
+          pictureAspect: 'noChange',
+          themeColor: '#ffffff',
+          manifest: {
+            name: 'Foundation + Gulp Boilerplate',
+            display: 'standalone',
+            orientation: 'notSet',
+            onConflict: 'override',
+            declared: true
+          },
+          assets: {
+            legacyIcon: false,
+            lowResolutionIcons: false
+          }
+        },
+        safariPinnedTab: {
+          pictureAspect: 'blackAndWhite',
+          threshold: 78.125,
+          themeColor: '#ffffff'
+        }
+      },
+      settings: {
+        scalingAlgorithm: 'Mitchell',
+        errorOnImageTooSmall: false,
+        readmeFile: false,
+        htmlCodeFile: false,
+        usePathAsIs: false
+      },
+      markupFile: 'favicon.json'
+    }, function() {
+      done();
+    });
+  }
+
+);
+
+/**
+ * Favicon Update Task. Check for updates on RealFaviconGenerator.
+ * @external '.realFavicon'
+ * @see {@link https://www.npmjs.com/package/gulp-real-favicon}
+ */
+gulp.task('favicon-update', function(done) {
+
+  const currentVersion = JSON.parse(fs.readFileSync('favicon.json')).version;
+
+  $.realFavicon.checkForUpdates(currentVersion, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
+
+});
+
+/**
+ * Watch Task. Watches for changes in JS and SCSS files.
+ * @external 'gulp.watch'
+ * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/API.md#gulpwatchglobs-opts-fn}
+ */
+gulp.task('watch', function(done) {
+  gulp.watch(`${config.src}/scss/**/*.scss`, gulp.parallel('styles'));
+  gulp.watch(`${config.src}/js/**/*.js`, gulp.parallel('scripts'));
+  done();
 });
 
 /**
@@ -134,4 +349,15 @@ gulp.task('fonts', function() {
  * @external 'gulp.task'
  * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
  */
-gulp.task('default', ['clean', 'scripts', 'styles']);
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel([
+      'favicon',
+      'fonts',
+      'images',
+      'scripts',
+      'styles'
+    ]),
+    'watch'
+  )
+);
